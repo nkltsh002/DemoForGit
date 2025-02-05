e7ad5f375f558ee2a287022aa6caade5859e87fc
Tshwanelo Nkalanga <tshwanelophawolethu@gmail.com>
2025-02-05 09:21:50 +0200

    Add demostration to the file

---
+++ test.R
@@ -0,0 +1,9 @@
+usethis:: use_git()
+
+# set your username and your email address
+username <- "Tshwanelo Nkalanga"  # replace NULL with e.g. "Miguel Rodo" (be sure to include the quotes)
+email <- "tshwanelophawolethu@gmail.com" # replace NULL with e.g. "miguel.rodo@uct.ac.za"
+stopifnot(!is.null(username)) # just checking you set this
+stopifnot(!is.null(email))
+gert::git_config_global_set("user.name",  username)
+gert::git_config_global_set("user.email", email)
